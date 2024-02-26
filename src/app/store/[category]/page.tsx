db967aee101dd04d16c4f89086c74a3be0884d3a
uriel <urielhernandezgarcia1@gmail.com>
2024-02-25 23:52:28 -0600

    "Como crear rutas en next"

---
+++ src/app/store/[category]/page.tsx
@@ -0,0 +1,13 @@
+interface CategoryProps {
+    params: {
+        category: string
+    }
+}
+
+export default function Category(porps: CategoryProps){
+    const { category } = porps.params
+    
+    return(
+        <h1>Categoria dinamica: {category} </h1>
+    )
+}
