09e938bf07ba74fb6d757968365053c4c62226be
uriel <urielhernandezgarcia1@gmail.com>
2024-03-01 23:49:03 -0600

    "Como usar sass"

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 
+
 const path = require('path')
 
-const nextConfig = {
-  sassOptions: {
+const nextConfig = { 
+    sassOptions: {
     includePaths: [path.join(__dirname, 'src/sass')],
     prependData: `@import "main.sass"`,
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
--- src/app/componentes/shared/Header/home/Hero/Hero.tsx
+++ src/app/componentes/shared/Header/home/Hero/Hero.tsx
@@ -1,6 +1,5 @@
 import styles from './Hero.module.sass'
 
-
 export const Hero = () => {
     
     
