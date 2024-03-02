aba13581b1947108f9df5f3663c15097ed374c9b
uriel <urielhernandezgarcia1@gmail.com>
2024-03-01 20:19:09 -0600

    "CSS Modules NextJS"

--- src/app/componentes/shared/Header/home/Hero/Hero.tsx
+++ src/app/componentes/shared/Header/home/Hero/Hero.tsx
@@ -1,7 +1,12 @@
+import styles from './Hero.module.css'
+
 export const Hero = () => {
+    
+    
     return(
-        <section>
-            <h1>Hero</h1>
+        <section className={styles.Hero}>
+            <h1>Future Worold</h1>
+            <h2> Empowering Your Tomorrow, Today!</h2>
         </section>
     )
 }
