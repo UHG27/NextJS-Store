f20eed1089ba8ea3298f3b8f9f68ba6b059eec49
uriel <urielhernandezgarcia1@gmail.com>
2024-03-04 21:33:56 -0600

    Manejo y optimizacion de imagenes

--- src/app/componentes/shared/Header/home/Description/Description.tsx
+++ src/app/componentes/shared/Header/home/Description/Description.tsx
@@ -1,9 +1,19 @@
+import styles from './Description.module.sass';
+import Image from 'next/image';
+
 export const Description = () => {
-    return(
-        <section>
-            <img src="/images/description.jpeg" alt="products marketplace" />
-            <h2>Description</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima atque esse provident quas. Tempore repellat dicta dolor tenetur? Labore itaque, numquam harum eligendi reiciendis sequi fuga odio? Esse, provident voluptatem?</p>
+    return (
+        <section className={styles.Description}>
+            <Image
+                src="/images/description.jpeg"
+                alt="products marketplace"
+                width={500}
+                height={300} 
+                />
+            <div>
+                <h2>Bring teh futur today</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima atque esse provident quas. Tempore repellat dicta dolor tenetur? Labore itaque, numquam harum eligendi reiciendis sequi fuga odio? Esse, provident voluptatem?</p>
+            </div>
         </section>
     )
 }
