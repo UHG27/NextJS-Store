f69d8e5f056a8d0087e2269a7715b152cf8bb95e
uriel <urielhernandezgarcia1@gmail.com>
2024-03-22 00:57:14 -0600

    Manejo de estado de carga con Loading

--- next.config.js
+++ next.config.js
@@ -3,11 +3,19 @@
 
 const path = require('path')
 
-const nextConfig = { 
-    sassOptions: {
+const nextConfig = {
+  sassOptions: {
     includePaths: [path.join(__dirname, 'src/sass')],
     prependData: `@import "main.sass"`,
   },
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'cdn.shopify.com',
+        protocol: 'https',
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
---
+++ src/app/loading.tsx
@@ -0,0 +1,8 @@
+import { Loader } from "./componentes/shared/Header/Shared/Loader"
+
+
+export default function name() {
+    return(
+        <h1>Cargando</h1>
+    )
+    }
